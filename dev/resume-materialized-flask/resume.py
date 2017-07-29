2f228fcac87db45f6f6c61c963ecd4a7c678f483
Jack Kenyon <jackenyon@gmail.com>
2017-07-28 20:22:16 -0400

    reqs
    requirements

---
+++ dev/resume-materialized-flask/resume.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+def render_resume():
+    return render_template('index.html')
+
+
+@app.route("/testing")
+def render_testing():
+    return render_template('testing.html')
+
+
+if __name__ == "__main__":
+    app.run()
