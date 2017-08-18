5cd1f79e560a65b7e8a56177be7d087e7a294f4a
Jack Kenyon <jackenyon@gmail.com>
2017-08-18 16:13:22 -0400

    Made a new testing page
    Test

--- app/resume.py
+++ app/resume.py
@@ -13,5 +13,10 @@ def render_testing():
     return render_template('testing.html')
 
 
+@app.route('/index-test')
+def render_hometest():
+    return render_template('index_testing.html')
+
+
 if __name__ == "__main__":
     app.run()
